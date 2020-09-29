acf3c18776c90681b8c1514c251873a5c7a3b859
Ferasico <ferasicoit@gmail.com>
2020-09-30 01:55:37 +0300

    some init msg

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+//let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+//
+//var password = alphabet[Int.random(in: 0...25)]+alphabet[Int.random(in: 0...25)]+alphabet[Int.random(in: 0...25)]+alphabet[Int.random(in:0...25)]+alphabet[Int.random(in:0...25)]+alphabet[Int.random(in:0...25)]
+//
+//print(password)
+
+//
+//func playSound(soundName: Int){
+//
+//    if soundName <= 100 && soundName >= 80 {
+//        print(" You Enter Larg Number: \(soundName)")
+//    } else if soundName <= 79 && soundName >= 50{
+//        print(" You Enter Medim Number: \(soundName)")
+//    }else{
+//      print(" You Enter Small Number: \(soundName)")
+//    }
+//
+//}
+//
+//playSound(soundName: 55)
+//playSound(soundName: 77)
+//playSound(soundName: 40)
+//playSound(soundName: 99)
+
+
+
+
---
+++ WeatherManger.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherManger.swift
+//  Clima
+//
+//  Created by Feras Al-Mutairi on 08/07/2020.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
